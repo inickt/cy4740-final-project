a8c1051af24aba6fa8a50637bd70575d1ebfb090
alon-neerman <47182779+alon-neerman@users.noreply.github.com>
2020-12-16 18:00:45 -0500

    Add files via upload

---
+++ src/stats.py
@@ -0,0 +1,22 @@
+
+from collections import defaultdict
+import json, sys
+
+def main(data):
+    tls_1_2 = 0
+    for label in data:
+        tls_count = defaultdict(int)
+        for request in data[label]:
+            tls_count[request['tls_version']] += 1
+        for tls_type in tls_count:
+            print(f"Label --> {label} --> TLS_TYPE --> {tls_type} --> NUMBER OF TLS_CONNECTIONS --> {tls_count[tls_type]}")
+        print("")
+
+
+if __name__ == '__main__':
+    json_data = sys.argv[1]
+    with open(json_data) as f:
+        data = json.load(f)
+
+    main(data)
+    
