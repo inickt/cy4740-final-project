f53fe096bae4c82629b9b020720597c2093dd160
Matt <odonnell.matt@northeastern.edu>
2020-12-16 20:49:52 -0500

    json-parse update

--- src/json-parse.py
+++ src/json-parse.py
@@ -1,24 +1,24 @@
 import json
 import sys
+
+# Renamed the dump file to the same name as the filter I used for simplicity
+
 def main():
     hosts= {}
 
-    f= open('cnn.json')
+    f= open(sys.argv[1]+'.json')
     data= json.load(f)
 
     # Pass name of json dump as command line argument
     for i in data[sys.argv[1]]:
-        for j in i['host']:
-            #print(i['host'])            
-            if i['host'] in hosts:
-                hosts[i['host']]= hosts[i['host']] + 1
-            else:
-                hosts[i['host']]= 1
+        if i['host'] in hosts:
+            hosts[i['host']]= hosts[i['host']] + 1
+        else:
+            hosts[i['host']]= 1
 
     with open('parsed.json', 'w') as out:
         json.dump(hosts, out)
 
-    #print(hosts)
     f.close()
 
 main()
