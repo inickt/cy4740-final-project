65c808254543700038a4e877cd40db1e7d27192f
Nick Thompson <git@nickt.dev>
2020-12-17 14:50:13 -0500

    Clean up domain graph

--- src/graph.py
+++ src/graph.py
@@ -1,46 +1,74 @@
 from collections import defaultdict
 import json
 import sys
+import os
 from pprint import pprint
 
 from pyvis.network import Network
 
 
-def main(data):
-    network = Network(height="100%", width="70%")
+def main(data, name):
+    # Set up network
+    network = Network(height="100%", width="100%", heading=name)
 
-    flattened_domains = defaultdict(lambda: 1)
+    # Count total number of visits
+    flattened_domains = defaultdict(int)
     for requests in data.values():
         for request in requests:
             flattened_domains[request["host"]] += 1
-    
-    pprint(flattened_domains)
 
+    # Add node for each domain
     for domain, count in flattened_domains.items():
-        network.add_node(domain, label=domain, shape="dot", value=count)
+        labels = defaultdict(int)
+        for label, requests in data.items():
+            for request in requests:
+                if request["host"] == domain:
+                    labels[label] += 1
+
+        hover_text = "<b>Sites:</b>"
+        for label, count in sorted(labels.items(), key=lambda kv: kv[1], reverse=True):
+            hover_text += f"<br>{label} - {count}"
+        network.add_node(
+            domain, label=domain, shape="dot", value=count, title=hover_text
+        )
 
     for label, requests in data.items():
-        network.add_node(label, label=label, shape="circle", group=label)
-        domains = defaultdict(lambda: 1)
+        # Count total requests out
+        domains = defaultdict(int)
         for request in requests:
             domains[request["host"]] += 1
+
+        # Add label node
+        hover_text = "<b>Domains:</b>"
+        for domain, count in sorted(
+            domains.items(), key=lambda kv: kv[1], reverse=True
+        ):
+            hover_text += f"<br>{domain} - {count}"
+        network.add_node(
+            label, label=label, shape="circle", group=label, title=hover_text
+        )
+
+        # Add edges
         for domain, count in domains.items():
             network.add_edge(label, domain, title=str(count), value=count)
 
+    # Create network graph
     network.force_atlas_2based(
         gravity=-155,
         central_gravity=0.035,
         spring_length=70,
         spring_strength=0.215,
         damping=0.55,
-        overlap=1
+        overlap=1,
     )
-    network.show_buttons(["physics"])
+    # network.show_buttons(["physics"])
     network.toggle_physics(True)
-    network.show("network.html")
+    network.show(f"{name}.html")
 
 
 if __name__ == "__main__":
-    with open(sys.argv[1]) as file:
+    filepath = sys.argv[1]
+    with open(filepath) as file:
         data = json.load(file)
-    main(data)
+    name = os.path.basename(filepath).replace(".json", "")
+    main(data, name)
